7f271d1e019296943cb8c22c390cc75451404dd1
Markpagso03 <pagsolinganmark04@gmail.com>
2025-02-12 16:38:01 +0800

    commit2

--- AeroMaster/AeroMaster/settings.py
+++ AeroMaster/AeroMaster/settings.py
@@ -116,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'frontend/static/'
-STATICFILES_DIRS = [BASE_DIR / 'kape_thana_web/static']
+STATIC_URL = str(BASE_DIR) + 'frontend/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
